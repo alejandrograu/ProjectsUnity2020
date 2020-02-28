a59558cdeaabd683be24f07df9e532977dcd7c19
Alejandrograu <AlejandroGrau@gmail.com>
2020-02-28 21:17:46 +0100

    Primer commit de Assets Vulcan de Unity3D

---
+++ Scripts/Impulse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulse : MonoBehaviour
+{
+	private Rigidbody rigidBody;
+	public float forceImpulse=1.0f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody=GetComponent<Rigidbody>();
+		// The initial impulse to be applied to every stone only
+		// when they are created
+		rigidBody.AddForce(Vector3.up*forceImpulse,ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
